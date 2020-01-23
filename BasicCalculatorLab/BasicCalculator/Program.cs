f6e76c3fdac5cc305feec9c7be574f76de25e764
twwilsoneod <59981796+twwilsoneod@users.noreply.github.com>
2020-01-23 16:47:14 -0600

    Update Program.cs
    Added Dew Point and Wind Chill calculations

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -6,19 +6,29 @@ namespace BasicCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** BASIC CALCULATOR ***");
+            Console.WriteLine("*** BASIC DEW POINT CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
+            Console.WriteLine("Enter Current Temperature");
 
             // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
+            int cTemp = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Relative Humidity");
+            int rh = int.Parse(Console.ReadLine());
 
-            int sum = firstNumber + secondNumber;
+            int dewPoint = cTemp - (100-rh)*9/25;
 
-            Console.WriteLine("The answer is " + sum);
+            Console.WriteLine("The Dew Point is " + dewPoint);
+
+            Console.WriteLine("*** BASIC WIND CHILL CALCULATOR ***");
+
+            Console.WriteLine("Enter the Wind Speed");
+
+            int windSpeed = int.Parse(Console.ReadLine());
+
+            double windChill = 35.74 + .6215 * cTemp - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * cTemp * Math.Pow(windSpeed, 0.16);
+
+            Console.WriteLine("The Wind Chill is " + windChill);
         }
     }
 }
